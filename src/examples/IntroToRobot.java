4962c0143dfd64af254d15d48df3ce48394ad71a
LanaDelSlay <gmiller1902@gmail.com>
2020-03-31 13:47:19 -0700

    Added functionality to create 4 squares!

--- src/examples/IntroToRobot.java
+++ src/examples/IntroToRobot.java
@@ -7,12 +7,15 @@ public class IntroToRobot {
 
 		Robot r2d2 = new Robot();
 		r2d2.penDown();
-		r2d2.setPenWidth(20);
+		r2d2.setPenWidth(5);
 		r2d2.setSpeed(5);
 		for (int i = 0; i < 4; i++) {
-			r2d2.move(100);
-			r2d2.turn(90);
+			r2d2.setRandomPenColor();
+			for (int o = 0; o < 4; o++) {
+				r2d2.move(150);
+				r2d2.turn(90);
+			}
+			r2d2.turn(-90);
 		}
-
 	}
 }
