bfc6af2301f196771e699888bd22f1c71c5a590d
LanaDelSlay <gmiller1902@gmail.com>
2020-04-01 11:32:33 -0700

    Finishing Exercises.

--- src/examples/Spiral.java
+++ src/examples/Spiral.java
@@ -7,11 +7,11 @@ public class Spiral {
 	public static void main(String[] args) {
 		Robot juneBot = new Robot("june");
 		juneBot.penDown();
-		juneBot.setSpeed(10);
+		juneBot.setSpeed(100);
 		for (int i = 0; i < 75; i++) {
 			juneBot.setRandomPenColor();
 			juneBot.move(5*i);
-			juneBot.turn(360/7); 
+			juneBot.turn(360/9); 
 			juneBot.setPenWidth(i);
 		}
 	}
--- src/exercises/NASACountdown.java
+++ src/exercises/NASACountdown.java
@@ -3,9 +3,22 @@ package exercises;
 /*
  * Count down a rocket launch!
  * 
- * Print the numbers from 10 to 1, then print “blastoff”. 
+ * Print the numbers from 10 to 1, then print â€œblastoffâ€�. 
  */
 
 public class NASACountdown {
 
-}
+	public static void main(String args[]) {
+		int time = 10;
+		System.out.println("T-Miuns " + (time) + " seconds");
+		for(int i = 1; i <= 9; i++) {
+			System.out.println((time - i) + " seconds");
+		}
+		
+		System.out.println("BLAASST OFFF!!"); 
+		
+	}
+	
+
+	
+	}
